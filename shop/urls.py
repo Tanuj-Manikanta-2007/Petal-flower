3b262744febfd683765ee56082a5e9691c63f175
Tanuj-Manikanta-2007 <pinnintitanuj@gmail.com>
2026-01-11 21:54:12 +0530

    Added shop login register

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 urlpatterns = [
-  path('',views.dashboard,name = 'shop_dashboard'),
+  path('',views.home,name = 'shop_home'),
+  path('dashboard/',views.dashboard,name = 'shop_dashboard'),
+  path('register/',views.shop_register,name = "shop_register"),
+  path('login/',views.shop_login,name = "shop_login"),
   
   
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from petalcart import models as pcmodel
+from django.contrib import messages
+from django.contrib.auth.models import Group
+from .forms import ShopRegisterForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 # Create your views here.
-def dashboard(request):
+def home(request):
   flowers = pcmodel.Flower.objects.all()
   shops = pcmodel.FlowerShop.objects.all()
   return render(request,"shop/home.html",{"flowers" : flowers,"shops" : shops})
+
+def shop_register(request):
+  if request.method == "POST":
+    form = ShopRegisterForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      shop_group = Group.objects.get(name = "ShopOwner")
+      user.groups.add(shop_group)
+      pcmodel.FlowerShop.objects.create(
+        owner = user,
+        shop_name = form.cleaned_data['shop_name'],
+        shop_address = form.cleaned_data['shop_address']
+      )
+      messages.success(request,"Shop registered successfully! Login now.")
+      #return redirect("shop_login")
+    else:
+      messages.error(request,"Please correct the erros below.")
+  else:
+    form = ShopRegisterForm()
+
+  return render(request,"shop/shop_register.html",{"form" : form})
+
+def shop_login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user  = authenticate(username = username,password = password)
+    if user:
+      if user.groups.filter(name = "ShopOwner").exists():
+        login(request,user)
+        messages.success(request,"Welcome back !")
+        return redirect("shop_home")
+      else:
+        messages.error(request,"You are not registered as a shop owner.")
+    else:
+      messages.error(request,"Invalid username or password ")
+  return render(request,"shop/shop_login.html")
+
+def dashboard(request):
+  return render(request,"shop/dashboard.html")
+    
