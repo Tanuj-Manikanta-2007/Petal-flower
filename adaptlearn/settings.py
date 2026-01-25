857e92925bd4da93f07baba55de9061863d81f41
Tanuj-Manikanta-2007 <pinnintitanuj@gmail.com>
2026-01-25 09:38:18 +0530

    database url added

--- adaptlearn/settings.py
+++ adaptlearn/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,15 +80,24 @@ WSGI_APPLICATION = 'adaptlearn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':  'petalcart',
+#         'USER' : 'postgres',
+#         'PASSWORD' : '1590',
+#          'HOST' : 'localhost',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'petalcart',
-        'USER' : 'postgres',
-        'PASSWORD' : '1590',
-         'HOST' : 'localhost',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
 
 
 # Password validation
