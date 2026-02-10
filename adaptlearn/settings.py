af22bcf16a27d701a38d9971553460f762e940f4
Tanuj-Manikanta-2007 <pinnintitanuj@gmail.com>
2026-02-10 13:49:18 +0530

    Changes setting.py for deployment

--- adaptlearn/settings.py
+++ adaptlearn/settings.py
@@ -16,12 +16,12 @@ PETAL_CART_DIR = BASE_DIR
 # ---------------------------------------
 # SECURITY
 # ---------------------------------------
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","TANUJ_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".onrender.com",
+    "*",
 ]
 
 # ---------------------------------------
@@ -112,7 +112,7 @@ USE_TZ = True
 # ---------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ---------------------------------------
